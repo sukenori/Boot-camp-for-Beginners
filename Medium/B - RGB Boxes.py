82e0a455d4a993850277fb713c08a55f6bcf97de
sukenori <sukenori@coo.net>
2022-12-09 20:36:22 +0900

    Easy 34まで

--- Medium/B - RGB Boxes.py
+++ Medium/B - RGB Boxes.py
@@ -2,7 +2,7 @@ r,g,b,n=map(int,input().split())
 import math
 c=0
 for i in range(int(n/r)+1):
-    for j in range(int(n/g)+1):
+    for j in range(int((n-r*i)/g),int(n/g)+1):
         if n>=r*i+g*j and (n-r*i-g*j)%b==0:
             c+=(n-r*i-g*j)//b
             print(n-r*i-g*j)
