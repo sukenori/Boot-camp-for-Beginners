3577f5f6ec56a527be53aedf0e7c3ffdd08b63ce
sukenori <sukenori@coo.net>
2022-12-16 22:43:02 +0900

    Easy 41まで

--- Medium/D - Banned K.py
+++ Medium/D - Banned K.py
@@ -1,9 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
-aset=set(a)
-adic={i:a.count(i) for i in list(aset) if a.count(i)>1}
-import math
-for i in list(a):
-    aidic=adic.copy()
-    if i in adic: aidic[i]=adic[i]-1
-    print(sum(math.comb(aidic[j],2) for j in adic))
+ad={}; sad=0
+for i in list(set(a)):
+    c=a.count(i)
+    ad[i]=c
+    sad+=c*(c-1)/2
+for i in a:
+    print(int(sad-ad[i]*(ad[i]-1)/2+(ad[i]-1)*(ad[i]-2)/2))
