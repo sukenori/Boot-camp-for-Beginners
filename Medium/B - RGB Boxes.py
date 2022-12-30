26a5268ad03146f0e4b1f9c884b8f997cfb8bc29
sukenori <sukenori@coo.net>
2022-12-30 09:03:32 +0900

    Medium 19まで

--- Medium/B - RGB Boxes.py
+++ Medium/B - RGB Boxes.py
@@ -1,9 +1,7 @@
 r,g,b,n=map(int,input().split())
-import math
 c=0
-for i in range(int(n/r)+1):
-    for j in range(int((n-r*i)/g),int(n/g)+1):
+for i in range(n//r+1):
+    for j in range(n//g+1):
         if n>=r*i+g*j and (n-r*i-g*j)%b==0:
-            c+=(n-r*i-g*j)//b
-            print(n-r*i-g*j)
+            c+=1
 print(c)
--- Medium/D - Banned K.py
+++ Medium/D - Banned K.py
@@ -1,9 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-ad={}; sad=0
-for i in list(set(a)):
-    c=a.count(i)
-    ad[i]=c
-    sad+=c*(c-1)/2
-for i in a:
-    print(int(sad-ad[i]*(ad[i]-1)/2+(ad[i]-1)*(ad[i]-2)/2))
+s=[0]*(n+1)
+for _ in a: s[_]+=1
+sc0=[]; sc1=[]
+import math
+for _ in s:
+    sc0.append(math.comb(_,2))
+    if _>0:
+        sc1.append(math.comb(_-1,2))
+    else: sc1.append(0)
+for _ in a:
+    print(sum(sc0[:_]+[sc1[_]]+sc0[_+1:]))
